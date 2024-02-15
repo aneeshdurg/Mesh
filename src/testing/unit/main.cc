eb3a695596015c83fac2035d1d5868c2a1993d9e
Aneesh Durg <aneesh@bodo.ai>
2024-02-15 16:50:22 -0600

    apply make format changes to new file

--- src/testing/unit/main.cc
+++ src/testing/unit/main.cc
@@ -4,6 +4,6 @@
 
 // TODO: on MacOS, gtest_main segfaults immediately, so we need this wrapper.
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
